7fb47a02a2ae974e59f7a1e62ebb3c72f2451bba
98emre <emre-98@live.se>
2023-07-07 17:08:26 +0200

    Less code and fix logic

--- computer.js
+++ computer.js
@@ -1,71 +1,85 @@
-
 // computer.js
 
-let computerNameElement = document.getElementById("computerName");
-let computerPriceElement = document.getElementById("computerPrice");
-let computerDescriptionElement = document.getElementById("computerDescription");
-let computerFeaturesContainer = document.getElementById("computerFeaturesContainer");
+import { fetchComputers } from "./api.js";
+
+const computerNameElement = document.getElementById("computerTitle");
+const computerPriceElement = document.getElementById("computerPrice");
+const computerDescriptionElement = document.getElementById("computerDescription");
+const computerFeaturesElement = document.getElementById("computerFeatures");
+const computerImageElement = document.getElementById("computerImage");
+const laptopSelectElement = document.getElementById("laptopSelect");
+
+let computer = {
+  id: 0,
+  title: "",
+  price: 0,
+  description: "",
+  features: [],
+  image: ""
+};
+
+let data = [];
 
 
-let computerName = ""
-let computerPrice = 0;
-let computerDescription = ""
-let computerFeatures = [] 
+updateComputerUI();
 
-function updateComputerUI(){
-    computerNameElement.textContent = computerName.toString();
-    computerPriceElement.textContent = computerPrice.toString();
-    computerDescriptionElement.textContent = computerDescription.toString();
 
-    computerFeaturesContainer.innerHTML = "";
+function updateComputerUI() {
+  computerNameElement.textContent = computer.title.toString();
+  computerPriceElement.textContent = computer.price.toString();
+  computerDescriptionElement.textContent = computer.description.toString();
+  computerImageElement.src = computer.image; 
 
-    computerFeatures.forEach(function(feature){
-        let featureElement = document.createElement("li");
-        featureElement.textContent = feature;
-        computerFeaturesContainer.appendChild(featureElement);
-    });
+  computerFeaturesElement.innerHTML = ""; // Make sure to reset
+
+  computer.features.forEach((feature) => {
+    const featureElement = document.createElement("li");
+    featureElement.textContent = feature;
+    computerFeaturesElement.appendChild(featureElement);
+  });
 }
 
-function getComputerName(){
-    return computerName;
+function setComputer(newComputer) {  
+    
+  computer.id = newComputer.id
+  computer.title = newComputer.title
+  computer.price = newComputer.price
+  computer.description = newComputer.description
+  computer.features = newComputer.specs
+  computer.image = newComputer.image 
 }
 
-function setComputerName(newComputerName){
-    computerName = newComputerName;
+function laptopOptions(computers) {
+  computers.forEach((computer) => {
+    const optionElement = document.createElement("option");
+    optionElement.value = computer.id;
+    optionElement.textContent = computer.title;
+    laptopSelectElement.appendChild(optionElement);
+  });
 }
 
-function getComputerPrice(){
-    return computerPrice;
-}
-
-function setComputerPrice(newComputerPrice){
-    computerPrice = newComputerPrice;
-}
-
-function getComputerDescription(){
-    return computerDescription;
-}
-
-function setComputerDescription(newComputerDescription){
-    computerDescription = newComputerDescription;
-}
-
-function getComputerFeatures(){
-    return computerFeatures;
-}
-
-function setComputerFeatures(newComputerFeatures){
-    computerFeatures = newComputerFeatures;
-}
-
-function addFeatures(item){
-    computerFeatures.push(item);
-}
+fetchComputers()
+  .then((result) => {
+    data = result; 
+    laptopOptions(data);
+  })
+  .catch((error) => {
+    console.error("Error fetching computers:", error);
+  });
 
 
-export { 
-    getComputerName, setComputerName, 
-    getComputerPrice, setComputerPrice,
-    getComputerDescription, setComputerDescription,
-    getComputerFeatures, setComputerFeatures,addFeatures
+
+laptopSelectElement.addEventListener("change", (event) => {
+    const selectedComputer = data.find(item => item.id === parseInt(event.target.value))
+    if(selectedComputer != null || selectedComputer != undefined){
+        setComputer(selectedComputer)
+        console.log(computer)
+        updateComputerUI()
+    }
+
+})
+
+
+export default {
+  setComputer,
 };
