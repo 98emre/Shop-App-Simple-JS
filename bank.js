2e2f01c6286d684c7224ec7a82c06c24b31d1396
98emre <emre-98@live.se>
2023-07-07 10:16:56 +0200

    bank JS fil
    It handle the bank section.

---
+++ bank.js
@@ -0,0 +1,73 @@
+
+// Bank.js
+
+let bankBalanceElement = document.getElementById("bankBalance");
+let amountOfLoanElement = document.getElementById("amountOfLoan");
+let amountOfLoan = 0;
+let bankBalance = 0;
+
+function getBankBalance(){
+    return bankBalance;
+}
+
+function setBankBalance(newBankBalanceValue){
+    bankBalance = newBankBalanceValue
+}
+
+function getAmountOfLoan(){
+    return amountOfLoan;
+}
+
+function setAmountOfLoan(newLoanValue){
+    amountOfLoan = newLoanValue
+}
+
+
+function updateBankUI(){
+    bankBalanceElement.textContent = bankBalance.toString();
+    amountOfLoanElement.textContent = amountOfLoan.toString();
+}
+
+updateBankUI();
+
+
+function getLoan() {
+    if (amountOfLoan > 0) {
+        alert("You already have a loan");
+        return;
+    }
+
+    var enterAmount = prompt("Enter the loan: ");
+    
+    if (enterAmount === null) {
+        return;
+    }
+
+    enterAmount = parseInt(enterAmount);
+
+    if (isNaN(enterAmount) || enterAmount <= 0) {
+        alert("Invalid input for the loan");
+        return;
+    }
+
+    if (enterAmount > bankBalance * 2) {
+        alert("Loan cannot exceed double your balance");
+        return;
+    }
+
+    bankBalance += enterAmount;
+    amountOfLoan += enterAmount;
+
+    document.querySelector(".repayLoan-button").removeAttribute("hidden");
+    updateBankUI();
+}
+
+
+
+
+let loanButton = document.querySelector("#loanButton");
+loanButton.addEventListener("click", getLoan);
+
+export { getAmountOfLoan, setAmountOfLoan, getBankBalance, setBankBalance, updateBankUI };
+
+
