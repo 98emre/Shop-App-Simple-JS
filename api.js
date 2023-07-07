c3240a526bc9a63b264d7a04f8a7eccf64d6fcd5
98emre <emre-98@live.se>
2023-07-07 10:19:02 +0200

    API Js file

---
+++ api.js
@@ -0,0 +1,14 @@
+
+// Api.js
+
+export async function fetchComputers() {
+    try {
+      const response = await fetch('data.json');
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching computers:', error);
+      return [];
+    }
+  }
+  
